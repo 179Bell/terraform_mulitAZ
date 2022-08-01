220546d227639de9473d70a2df11ba4581cc513f
179Bell <shingo.msdf@gmail.com>
2022-08-01 21:50:36 +0900

    amiをdata_sorceを使って取得

--- env/security_group.tf
+++ env/security_group.tf
@@ -1,8 +1,8 @@
 module "security_group" {
-    source = "../module/security_group"
+  source = "../module/security_group"
 
-    vpc_id            = module.network.vpc
-    public_subnet_id  = module.network.public_subnet[*].id
-    private_subnet_id = module.network.private_subnet[*].id
-    name              = "${var.name}-${terraform.workspace}"
+  vpc_id            = module.network.vpc
+  public_subnet_id  = module.network.public_subnet[*].id
+  private_subnet_id = module.network.private_subnet[*].id
+  name              = "${var.name}-${terraform.workspace}"
 }
